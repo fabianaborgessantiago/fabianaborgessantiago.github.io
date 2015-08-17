a86a649aaef702d52fa474f1ea5bb9da9765e575
Renato Machado <renato.zehh@gmail.com>
2015-08-17 10:22:13 -0300

    removendo site do ar

--- js/directives.js
+++
@@ -1,8 +0,0 @@
-app.directive('backImg', function() {
-    return function(scope, element, attrs){
-        var url = attrs.backImg;
-        element.css({
-            'background-image': 'url(' + url +')'
-        });
-    };
-});
--- js/home/controller.js
+++
@@ -1,7 +0,0 @@
-app.controller('homeController', function($scope){
-    
-    //todo:buscar imagem do db
-    $scope.bgImg = "/img/fabi-bg.jpg";
-    console.log($scope.bgImg);
-    
-});
